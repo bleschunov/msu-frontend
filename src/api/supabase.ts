d220f7b5a7a6ecd02486f8ac7bfac95396e7bfbc
Dmitry Bleschunov <bleshunov5b@yandex.ru>
2023-08-19 17:00:29 +0300

    add comment about supabase api key

--- src/api/supabase.ts
+++ src/api/supabase.ts
@@ -2,6 +2,7 @@ import {createClient, User} from "@supabase/supabase-js";
 
 const supabase = createClient(
     "https://jkhlwowgrekoqgvfruhq.supabase.co",
+    // Это АПИ ключ из supabase, который можно палить. Есть ещё другой ключ, который палить нельзя
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpraGx3b3dncmVrb3FndmZydWhxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTA0NTcwMzQsImV4cCI6MjAwNjAzMzAzNH0.5PLTJ0CaG1n9EdWC-M3B2-p9WK6VCdxTr7eNnPON4oU"
 )
 
