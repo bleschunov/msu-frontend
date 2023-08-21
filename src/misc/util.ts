4a59abdb4fd47c356d13ad52715346ff6b5b4a01
dmitriyPoteryaev <dim.poteryaeff@yandex.ru>
2023-08-21 14:36:13 +0300

    add method locale

--- src/misc/util.ts
+++ src/misc/util.ts
@@ -1,20 +1,22 @@
 import {ReactNode} from "react";
 import moment from "moment";
+import 'moment/locale/ru'
+ 
 
 const getBaseUrl = (): string => {
     if (process.env.NODE_ENV === "production") {
         return "https://msu-backend-dev.fly.dev/api/v1"
     }
 
-    return "http://0.0.0.0:8080/api/v1"
+    return "https://msu-backend-dev.fly.dev/api/v1"
 }
 
 const getLastN = (n: number, arr: ReactNode[]) => {
     return arr.slice(Math.max(arr.length - n, 0))
 }
 
-const formatDate = (date: Date): string => {
-    return moment(date).calendar()
+const formatDate = (date: Date): any => {
+    return moment(date).locale("ru").calendar()
 }
 
 export {
