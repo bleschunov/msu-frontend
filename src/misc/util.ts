a077ebc4c33d6d49b730603fad24313a38f85e07
dmitriyPoteryaev <dim.poteryaeff@yandex.ru>
2023-08-22 18:56:36 +0300

    change logic with useEffect

--- src/misc/util.ts
+++ src/misc/util.ts
@@ -6,7 +6,7 @@ const getBaseUrl = (): string => {
         return "https://msu-backend-dev.fly.dev/api/v1"
     }
 
-    return "http://0.0.0.0:8080/api/v1"
+    return "https://msu-backend-dev.fly.dev/api/v1"
 }
 
 const getLastN = (n: number, arr: ReactNode[]) => {
