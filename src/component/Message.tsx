a8beece9e5bc62ac0356890b6a17dad0765742c7
Dmitry Bleschunov <bleshunov5b@yandex.ru>
2023-08-28 10:31:11 +0300

    update eslint

--- src/component/Message.tsx
+++ src/component/Message.tsx
@@ -101,7 +101,7 @@ export const createMessage = (messageModel: MessageModel): ReactNode => {
         markModel={messageModel.mark && (messageModel.mark.length === 0 ? undefined : messageModel.mark[0])}
         src={src}
         messageId={messageModel.id}
-        direction={messageModel.answer != undefined ? "incoming" : "outgoing"}
+        direction={messageModel.answer != undefined ? "incoming" : "outgoing"} // eslint-disable-line
         key={messageModel.id}
     >
         <Text>{messageContent}</Text>
