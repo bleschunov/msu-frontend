e7008752e166dd87e710727219e4baa150f2e49f
Kate Kovtun <katerina.kvtn.skh@gmail.com>
2023-09-11 14:23:13 +0300

    feat: add profile menu and clear button

--- src/component/Chat.tsx
+++ src/component/Chat.tsx
@@ -1,14 +1,14 @@
-import { useQuery } from "react-query"
-import { useContext, useEffect, useRef, useState } from "react"
 import { Button, Flex } from "@chakra-ui/react"
+import { getOrCreateChat } from "api/chatApi"
 import InputGroup from "component/InputGroup"
-import { createMessage, Message } from "component/Message"
+import { Message, createMessage } from "component/Message"
+import { ModeContext, ModeContextI } from "context/modeContext"
+import { UserContext } from "context/userContext"
 import { getLastN } from "misc/util"
 import ChatModel from "model/ChatModel"
-import { getOrCreateChat } from "api/chatApi"
-import { UserContext } from "context/userContext"
 import MessageModel from "model/MessageModel"
-import { ModeContext, ModeContextI } from "context/modeContext"
+import { useContext, useEffect, useRef, useState } from "react"
+import { useQuery } from "react-query"
 import { useCreateMessage } from "service/messageService"
 import { usePrediction } from "service/predictionService"
 
@@ -17,7 +17,8 @@ function Chat() {
     const chatRef = useRef<HTMLDivElement | null>(null)
     const [query, setQuery] = useState("")
     const user = useContext(UserContext)
-    const { shownMessageCount, setShownMessageCount } = useContext<ModeContextI>(ModeContext)
+    const { shownMessageCount, setShownMessageCount } =
+        useContext<ModeContextI>(ModeContext)
 
     const { data: chat, status } = useQuery<ChatModel>("chat", () => {
         return getOrCreateChat(user.id)
@@ -27,43 +28,81 @@ function Chat() {
     const predictionMutation = usePrediction()
 
     useEffect(() => {
-        window.scroll({ top: chatRef.current?.offsetHeight, behavior: "smooth" })
+        window.scroll({
+            top: chatRef.current?.offsetHeight,
+            behavior: "smooth",
+        })
     }, [chat?.message?.length])
 
     const handleSubmit = async () => {
         if (query !== "" && chat) {
-            const { id: queryMessage } = await messageCreateMutation.mutateAsync({ chat_id: chat.id, query } as MessageModel)
-            const { answer, sql, table } = await predictionMutation.mutateAsync({ query })
+            const { id: queryMessage } =
+                await messageCreateMutation.mutateAsync({
+                    chat_id: chat.id,
+                    query,
+                } as MessageModel)
+            const { answer, sql, table } = await predictionMutation.mutateAsync(
+                {
+                    query,
+                }
+            )
             messageCreateMutation.mutate({
                 chat_id: chat.id,
                 answer: answer,
                 sql: sql,
                 table: table,
-                connected_message_id: queryMessage
+                connected_message_id: queryMessage,
             } as MessageModel)
             setQuery("")
         }
     }
 
     const handleShowMore = () => {
-        setShownMessageCount(lastN => lastN + 10)
+        setShownMessageCount((lastN) => lastN + 10)
     }
 
-    const isLoading = predictionMutation.isLoading
-        || messageCreateMutation.isLoading
-        || status === "loading"
+    const isLoading =
+        predictionMutation.isLoading ||
+        messageCreateMutation.isLoading ||
+        status === "loading"
 
     return (
         <Flex direction="column" p="10" h="full" gap={10} ref={chatRef}>
-            {chat && !!chat.message?.length && chat.message.length > shownMessageCount
-                && <Button colorScheme="blue" variant="link" onClick={handleShowMore}>Предыдущие сообщения</Button>}
-            <Flex direction="column" gap="5" flexGrow="1" ref={messageWindowRef}>
-                {chat && !!chat.message?.length && getLastN(shownMessageCount, chat.message.map((message) => createMessage(message)))}
-            </Flex>
-            {chat && !chat.message?.length &&
-                <Message direction='incoming' messageId={-1} src={"/image/avatar/bot.png"} callback={false}>
+            <>
+                {chat &&
+                !!chat.message?.length && chat.message.length > shownMessageCount && (
+                    <Button
+                        colorScheme="blue"
+                        variant="link"
+                        onClick={handleShowMore}
+                    >
+                        Предыдущие сообщения
+                    </Button>
+                )}
+                <Flex
+                    direction="column"
+                    gap="5"
+                    flexGrow="1"
+                    ref={messageWindowRef}
+                >
+                    {chat &&
+                    !!chat.message?.length &&
+                    getLastN(
+                        shownMessageCount,
+                        chat.message.map((message) => createMessage(message))
+                    )}
+                </Flex>
+                {chat && !chat.message?.length && (
+                    <Message
+                        direction="incoming"
+                        messageId={-1}
+                        src={"/image/avatar/bot.png"}
+                        callback={false}
+                    >
                     Какой у вас запрос?
-                </Message>}
+                    </Message>
+                )}
+            </>
             <InputGroup
                 disabled={isLoading}
                 value={query}
--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -1,21 +1,126 @@
-import React from "react"
-import { Button, HStack } from "@chakra-ui/react"
-import { useQueryClient } from "react-query"
+import { Avatar, Button, Flex, HStack, Menu, MenuButton, MenuGroup, MenuItem, MenuList, useToast } from "@chakra-ui/react"
+import { User } from "@supabase/supabase-js"
+import { getOrCreateChat } from "api/chatApi"
+import { getUser, signOut } from "api/supabase"
 import Logo from "component/Logo"
-import { signOut } from "api/supabase"
+import { UserContext } from "context/userContext"
+import ChatModel from "model/ChatModel"
+import React, { useContext, useEffect, useState } from "react"
+import { AiOutlineDown } from "react-icons/ai"
+import { useQuery, useQueryClient } from "react-query"
+import { useClearMessage } from "service/messageService"
 
 const Header = () => {
     const queryClient = useQueryClient()
+    const user = useContext(UserContext)
 
     const handleSignOut = () => {
         signOut()
         queryClient.clear()
     }
 
+    const toast = useToast()
+    const toastIdRef = React.useRef<string | number | undefined>()
+    const [seconds, setSeconds] = useState<number>(0)
+    const [isTimerActive, setTimerActive] = useState(false)
+
+    function addSuccefullToast() {
+        toastIdRef.current = toast({
+            title: "Чат успешно очищен",
+            description: "Все сообщения из чата удалены",
+            status: "success",
+            position: "bottom-right",
+            isClosable: true
+        })
+    }
+
+    useEffect(() => {
+        if (!seconds) {
+            if (isTimerActive) {
+                addSuccefullToast()
+                setTimerActive(false)
+            }
+            return
+        }
+
+        const intervalId = setInterval(() => {
+            setSeconds(seconds - 1)
+        }, 1000)
+
+        return () => clearInterval(intervalId)
+    }, [seconds, addSuccefullToast])
+
+    function addWarningToast() {
+        toastIdRef.current = toast({ 
+            title: "Очистка чата",
+            description:
+            <Flex direction="column">
+                Вы решили удалить все сообщения из чата
+                <Button 
+                    onClick={() => {
+                        setSeconds(0)
+                        setTimerActive(false)
+                        toast.close(toastIdRef.current!)
+                    }}
+                    alignSelf="flex-end"
+                    style={{
+                        marginLeft: 150
+                    }}
+                    type="button" 
+                    variant="link"
+                    colorScheme="black"
+                >
+                    Отменить
+                </Button>
+            </Flex>,
+            status: "warning",
+            position: "bottom-right",
+            isClosable: false,
+            duration: 5000, })
+    }
+
+    const clearMessagesMutation = useClearMessage()
+    const { data: chat } = useQuery<ChatModel>("chat", () => {
+        return getOrCreateChat(user.id)
+    })
+
+    const handleClear = async () => {
+        if (chat) {
+            clearMessagesMutation.mutate({
+                id: chat.id,
+                user_id: chat.user_id,
+            } as ChatModel)
+        }
+    }
+
+
     return (
-        <HStack bg="gray.100" h="100px" flexShrink="0" justify="space-between" px="10" py="5">
+        <HStack bg="gray.100" h="100px" flexShrink="0" justify="space-between" px="10" py="5" position="sticky" w="100%">
             <Logo />
-            <Button colorScheme="blue" variant="outline" onClick={handleSignOut}>Выйти</Button>
+            <HStack>
+                <Button 
+                    variant="outline" 
+                    colorScheme="blue"
+                    onClick={() => {
+                        addWarningToast()
+                        setTimerActive(true)
+                        setSeconds(5)
+                        handleClear()
+                    }}
+                >
+                    Очистить чат
+                </Button>
+                <Menu>
+                    <MenuButton as={Button} rightIcon={<AiOutlineDown/>} variant="link">
+                        <Avatar></Avatar>
+                    </MenuButton>
+                    <MenuList>
+                        <MenuGroup title={user.email}>
+                            <MenuItem onClick={handleSignOut}>Log out</MenuItem>
+                        </MenuGroup>
+                    </MenuList>
+                </Menu>
+            </HStack>
         </HStack>
     )
 }
