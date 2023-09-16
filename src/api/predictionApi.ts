fe68b778253cfb42c0af080b375d9d3343c7d3a4
Syrnnik <sakh.alexandr@gmail.com>
2023-09-16 20:31:54 +0300

    hotfix: chat pdf prediction request url

--- src/api/predictionApi.ts
+++ src/api/predictionApi.ts
@@ -8,7 +8,7 @@ const getDatastepPrediction = (body: Omit<QueryModel, "chat_id">): Promise<Messa
 
 const getChatPdfPrediction = (body: Omit<QueryModel, "chat_id">): Promise<MessageModel> => {
     return axiosClient.postForm(
-        "/v1/chat_pdf/prediction",
+        "/chat_pdf/prediction",
         { file: body.file },
         { params: { query: body.query } }
     ).then(response => ({ answer: response.data } as MessageModel))
