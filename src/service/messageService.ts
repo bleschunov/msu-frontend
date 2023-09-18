f52ba263cbce8da9f577527f8c7fe0796daebdc1
Kate Kovtun <katerina.kvtn.skh@gmail.com>
2023-09-18 23:53:32 +0300

    feat: add mutation for clearing messages

--- src/service/messageService.ts
+++ src/service/messageService.ts
@@ -1,11 +1,10 @@
-import { useMutation } from "react-query"
-import { useContext } from "react"
-import { createMessage as createMessageApi } from "api/messageApi"
-import { clearMessages as clearMessagesApi } from "api/messageApi"
-import MessageModel from "model/MessageModel"
-import ChatModel from "model/ChatModel"
+import { clearMessages as clearMessagesApi, createMessage as createMessageApi } from "api/messageApi"
 import queryClient from "api/queryClient"
 import { ModeContext, ModeContextI } from "context/modeContext"
+import ChatModel from "model/ChatModel"
+import MessageModel from "model/MessageModel"
+import { useContext } from "react"
+import { useMutation } from "react-query"
 
 const updateMessagesInChat = (previousChat: ChatModel, newMessage: MessageModel) => {
     previousChat.message?.push(newMessage)
@@ -36,6 +35,32 @@ const useCreateMessage = () => {
     })
 }
 
+const useClearMessages = () => {
+    const { setShownMessageCount } = useContext<ModeContextI>(ModeContext)
+
+    return useMutation(clearMessagesApi, {
+        onMutate: async () => {
+            await queryClient.cancelQueries("message")
+            const previousChat = queryClient.getQueryData<ChatModel>("chat")
+            if (previousChat) {
+                previousChat.message = []
+                queryClient.setQueriesData<ChatModel>("chat", previousChat)
+            }
+            return {
+                previousChat,
+            }
+        },
+        onError: (_error, _currentMark, context) => {
+            queryClient.setQueriesData("chat", context?.previousChat)
+        },
+        onSettled: () => {
+            setShownMessageCount((lastN: number) => lastN + 1)
+            queryClient.invalidateQueries("chat")
+        },
+    })
+}
+
 export {
-    useCreateMessage,
+    useClearMessages,
+    useCreateMessage
 }
