a151b88bfb3b0cc0fef096de09d869badf0a8821
Syrnnik <sakh.alexandr@gmail.com>
2023-09-20 23:14:16 +0300

    feat: add source prop to chat model

--- src/model/ChatModel.ts
+++ src/model/ChatModel.ts
@@ -1,10 +1,12 @@
 import MessageModel from "model/MessageModel"
+import SourceModel from "model/SourceModel"
 
 interface ChatModel {
     id: number
     user_id: string
     message?: MessageModel[]
     created_at: string
+    source?: SourceModel[]
 }
 
 export default ChatModel
