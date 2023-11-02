17f9feb83228e32cfe5d4ef7f07d918ac894d555
Dmitry Bleschunov <bleshunov5b@yandex.ru>
2023-11-02 21:39:30 +0600

    in progress

---
+++ src/model/DatabasePredictionConfigModel.ts
@@ -0,0 +1,9 @@
+interface DatabasePredictionConfigModel {
+    is_sql_description: boolean
+    is_check_data: boolean
+    is_alternative_questions: boolean
+}
+
+export type {
+    DatabasePredictionConfigModel
+}
