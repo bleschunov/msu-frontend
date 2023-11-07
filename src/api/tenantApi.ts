0acdfd06def0120a0e4bfe63bbfd4499a2a1baf8
Kate Kovtun <katerina.kvtn.skh@gmail.com>
2023-11-07 20:45:20 +0300

    feat: update create_tenant_with_user_id

--- src/api/tenantApi.ts
+++ src/api/tenantApi.ts
@@ -1,8 +1,8 @@
 import axiosClient from "api/axiosClient"
 import { UserModel } from "model/UserModel"
 
-const create_tenant_with_user_id = async (userId: string): Promise<UserModel> => {
-    return axiosClient.post(`/tenant/${userId}`)
+const create_tenant_with_user_id = async (user_id: string, email: string): Promise<UserModel> => {
+    return axiosClient.post("/tenant", { user_id, email })
 }
 
 export {
