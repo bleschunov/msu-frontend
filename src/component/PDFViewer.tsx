7f6b0a675d7854f3bd94ed9956ccb607ec2154ed
IT-Kitty <katerina.kvtn.skh@gmail.com>
2023-11-21 23:24:40 -0800

    feat: add sidebar for pdf viewer

--- src/component/PDFViewer.tsx
+++ src/component/PDFViewer.tsx
@@ -6,7 +6,7 @@ import { FC, useEffect } from "react"
 // Import styles
 import "@react-pdf-viewer/core/lib/styles/index.css"
 import "@react-pdf-viewer/default-layout/lib/styles/index.css"
-import { VStack } from "@chakra-ui/react"
+import { Box, Drawer, DrawerContent, VStack, useColorModeValue, useDisclosure } from "@chakra-ui/react"
 
 interface IPDFViewer {
     fileUrl: string
@@ -23,20 +23,31 @@ export const PDFViewer: FC<IPDFViewer> = ({ page, fileUrl }) => {
         jumpToPage(page)
     }, [page, jumpToPage])
 
+    const { isOpen, onClose } = useDisclosure()
+
     return (
         <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js">
-            <VStack
-                width={1000}
-                height={1000}
-            >
-                <Viewer
-                    fileUrl={fileUrl}
-                    plugins={[
-                        defaultLayoutPluginInstance,
-                        pageNavigationPluginInstance,
-                    ]}
-                    defaultScale={SpecialZoomLevel.PageFit}
-                />
+            <VStack>
+                <Box minH="100vh" minW="42vw">
+                    <Drawer
+                        isOpen={isOpen}
+                        placement="left"
+                        onClose={onClose}
+                        returnFocusOnClose={false}
+                        size="full">
+                        <DrawerContent />
+                    </Drawer>
+                    <Box ml={{ base: 0, md: 0 }}>
+                        <Viewer
+                            fileUrl={fileUrl}
+                            plugins={[
+                                defaultLayoutPluginInstance,
+                                pageNavigationPluginInstance,
+                            ]}
+                            defaultScale={SpecialZoomLevel.PageFit}
+                        />
+                    </Box>
+                </Box>
             </VStack>
         </Worker>
     )
