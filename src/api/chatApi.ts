59bfbe3d9e8311e77fa6e00b24862e4482b67a13
Dmitry Bleschunov <bleshunov5b@yandex.ru>
2024-01-29 20:38:28 +0600

    change sort function for messages

--- src/api/chatApi.ts
+++ src/api/chatApi.ts
@@ -1,7 +1,6 @@
 import { AxiosError } from "axios"
 import axiosClient from "api/axiosClient"
 import ChatModel from "model/ChatModel"
-import { sortDate } from "misc/util"
 
 const getChat = async (userId: string): Promise<ChatModel> => {
     const { data: chatModel } = await axiosClient.get(`/chat/${userId}`)
@@ -17,7 +16,7 @@ const getOrCreateChat = async (userId: string): Promise<ChatModel> => {
     try {
         const chat = await getChat(userId)
         chat.messages.sort(
-            (messageA, messageB) => sortDate(messageA.created_at, messageB.created_at, false)
+            (messageA, messageB) => messageA.id - messageB.id
         )
         return chat
     } catch (e) {
